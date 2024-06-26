0467e94e5a7944d487f9055995d83f515c5dae00
Asher <36462882+asherrf@users.noreply.github.com>
2024-06-26 17:15:25 -0400

    Update pidecodeplugin.js

--- plugins/pidecodeplugin.js
+++ plugins/pidecodeplugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'PI Decode',
-    version: '1.0',
+    version: '1.01',
     author: '.asherr',
     frontEndPath: 'PiDecode/PiDecodePlugin.js'
 }
